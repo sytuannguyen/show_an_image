d6384eb4d2f14a42396148ebd52f3ca2cfa84845
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2023-11-03 17:06:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ st.title('Image Enhancement App')
 # Upload image through file uploader
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
-if uploaded_file is None:
-    uploaded_file = 'Tolkien Fanart.png'
-    
 # Function to display the uploaded image
 def display_image(image):
     st.image(image, caption='Uploaded Image.', use_column_width=True)
@@ -29,11 +26,16 @@ if uploaded_file is not None:
     brightness = st.slider("Brightness", 0.0, 2.0, 1.0)
     contrast = st.slider("Contrast", 0.0, 2.0, 1.0)
     saturation = st.slider("Saturation", 0.0, 2.0, 1.0)
+    grayscale = st.checkbox("Convert to Black & White")
 
     # Apply enhancements to the image
     enhanced_image = ImageEnhance.Brightness(uploaded_image).enhance(brightness)
     enhanced_image = ImageEnhance.Contrast(enhanced_image).enhance(contrast)
     enhanced_image = ImageEnhance.Color(enhanced_image).enhance(saturation)
+
+    # Convert to black and white if the checkbox is selected
+    if grayscale:
+        enhanced_image = enhanced_image.convert("L")
 
     # Display the enhanced image
     st.subheader("Enhanced Image")
