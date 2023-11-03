63e9b16c92f6874fee909b701ebd4864a2ee100a
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2023-11-03 17:11:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,12 @@ from PIL import Image, ImageEnhance
 # Streamlit app title
 st.title('Image Enhancement App')
 
-# Upload image through file uploader
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+# Upload image through file uploader with a default value
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"], default="Tolkien Fanart.png")
 
-if uploaded_file is None:
-    uploaded_filed = 'Tolkien Fanart.png'
 # Function to display the uploaded image
-def display_image(image):
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+def display_image(image, zoom):
+    st.image(image, caption='Uploaded Image.', use_column_width=True, clamp=True, width=int(image.width * zoom))
     st.write("")
     return image
 
@@ -20,8 +18,10 @@ def display_image(image):
 if uploaded_file is not None:
     # Read the image using PIL
     image = Image.open(uploaded_file)
-    # Display the uploaded image
-    uploaded_image = display_image(image)
+    # Image zoom options
+    zoom_level = st.slider("Zoom Level", min_value=0.1, max_value=3.0, value=1.0, step=0.1)
+    # Display the uploaded image with zoom
+    uploaded_image = display_image(image, zoom_level)
 
     # Image enhancement options
     st.subheader("Image Enhancement Options")
@@ -39,9 +39,50 @@ if uploaded_file is not None:
     if grayscale:
         enhanced_image = enhanced_image.convert("L")
 
-    # Display the enhanced image
+    # Display the enhanced image with zoom
     st.subheader("Enhanced Image")
-    st.image(enhanced_image, caption='Enhanced Image.', use_column_width=True)
+   
+# Import required libraries
+import streamlit as st
+from PIL import Image, ImageEnhance
 
-else:
-    st.write("Please upload an image.")
+# Streamlit app title
+st.title('Image Enhancement App')
+
+# Upload image through file uploader with a default value
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"], default="Tolkien Fanart.png")
+
+# Function to display the uploaded image
+def display_image(image, zoom):
+    st.image(image, caption='Uploaded Image.', use_column_width=True, clamp=True, width=int(image.width * zoom))
+    st.write("")
+    return image
+
+# Check if an image has been uploaded
+if uploaded_file is not None:
+    # Read the image using PIL
+    image = Image.open(uploaded_file)
+    # Image zoom options
+    zoom_level = st.slider("Zoom Level", min_value=0.1, max_value=3.0, value=1.0, step=0.1)
+    # Display the uploaded image with zoom
+    uploaded_image = display_image(image, zoom_level)
+
+    # Image enhancement options
+    st.subheader("Image Enhancement Options")
+    brightness = st.slider("Brightness", 0.0, 2.0, 1.0)
+    contrast = st.slider("Contrast", 0.0, 2.0, 1.0)
+    saturation = st.slider("Saturation", 0.0, 2.0, 1.0)
+    grayscale = st.checkbox("Convert to Black & White")
+
+    # Apply enhancements to the image
+    enhanced_image = ImageEnhance.Brightness(uploaded_image).enhance(brightness)
+    enhanced_image = ImageEnhance.Contrast(enhanced_image).enhance(contrast)
+    enhanced_image = ImageEnhance.Color(enhanced_image).enhance(saturation)
+
+    # Convert to black and white if the checkbox is selected
+    if grayscale:
+        enhanced_image = enhanced_image.convert("L")
+
+    # Display the enhanced image with zoom
+    st.subheader("Enhanced Image")
+   
