dc0fcbce2a061f8b8d61b1b8068a42c1b1c49d48
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2023-11-03 16:59:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # Import required libraries
 import streamlit as st
-from PIL import Image, ImageEnhance, ImageOps
+from PIL import Image, ImageEnhance
 
 # Streamlit app title
-st.title('Image Loader App')
+st.title('Image Enhancement App')
 
 # Upload image through file uploader
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
@@ -12,44 +12,29 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg
 def display_image(image):
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
-    return
-
-# Function to enhance the image based on user options
-def enhance_image(image, enhancement_factor, rotation_angle, flip_horizontal, flip_vertical):
-    # Enhance image brightness and contrast
-    enhancer = ImageEnhance.Brightness(image)
-    image = enhancer.enhance(enhancement_factor)
-    
-    # Rotate image
-    image = image.rotate(rotation_angle)
-    
-    # Flip image horizontally and vertically based on user choice
-    if flip_horizontal:
-        image = ImageOps.mirror(image)
-    if flip_vertical:
-        image = image.transpose(Image.FLIP_TOP_BOTTOM)
-    
     return image
 
 # Check if an image has been uploaded
 if uploaded_file is not None:
     # Read the image using PIL
-    original_image = Image.open(uploaded_file)
-    
-    # User options for image enhancement
-    enhancement_factor = st.slider('Enhance Brightness/Contrast:', 0.1, 2.0, 1.0)
-    rotation_angle = st.slider('Rotate Image (degrees):', 0, 360, 0)
-    flip_horizontal = st.checkbox('Flip Horizontal')
-    flip_vertical = st.checkbox('Flip Vertical')
-    
-    # Enhance the image based on user options
-    enhanced_image = enhance_image(original_image, enhancement_factor, rotation_angle, flip_horizontal, flip_vertical)
-    
-    # Display the original and enhanced images
-    st.subheader('Original Image:')
-    display_image(original_image)
-    
-    st.subheader('Enhanced Image:')
-    display_image(enhanced_image)
+    image = Image.open(uploaded_file)
+    # Display the uploaded image
+    uploaded_image = display_image(image)
+
+    # Image enhancement options
+    st.subheader("Image Enhancement Options")
+    brightness = st.slider("Brightness", 0.0, 2.0, 1.0)
+    contrast = st.slider("Contrast", 0.0, 2.0, 1.0)
+    saturation = st.slider("Saturation", 0.0, 2.0, 1.0)
+
+    # Apply enhancements to the image
+    enhanced_image = ImageEnhance.Brightness(uploaded_image).enhance(brightness)
+    enhanced_image = ImageEnhance.Contrast(enhanced_image).enhance(contrast)
+    enhanced_image = ImageEnhance.Color(enhanced_image).enhance(saturation)
+
+    # Display the enhanced image
+    st.subheader("Enhanced Image")
+    st.image(enhanced_image, caption='Enhanced Image.', use_column_width=True)
+
 else:
     st.write("Please upload an image.")
